8d688fbca0685653a8e6ab097fe40c3416254ee9
Aidan11894 <aidanmcf118@hotmail.co.uk>
2024-12-18 18:00:03 +0000

    Auto-commit on 18-12-2024 18:00:03

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
 #include <iostream>
 #include <ostream>
 
-int main() {
-
-  int x = 0;
-
-  if (x == 5) {
-    std::cout << "x is 5" << std::endl;
-  } else {
-    std::cout << "x is not 5" << std::endl;
-  }
-
-  return 0;
-}
+int main() { return 0; }
